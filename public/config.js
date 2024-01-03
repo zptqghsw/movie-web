615f009ce67e4127e50c1c1b330a5ef44904733a
zptqghsw <83914995+zptqghsw@users.noreply.github.com>
2024-01-03 11:44:43 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,9 +1,9 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  VITE_CORS_PROXY_URL: "https://movieproxy.hohoo.fun",
+  VITE_CORS_PROXY_URL: "CHANGEME",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "139200d05a3be498f2d6725cefa752e4",
+  VITE_TMDB_READ_API_KEY: "CHANGEME",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
